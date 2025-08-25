86a183598c1b2baa983af55581db58c498cf493e
Facundo Deseff <facunditogustavo@gmail.com>
2025-08-24 22:27:43 -0300

    subo practico

--- trabajo practico1.py
+++ trabajo practico1.py
@@ -10,19 +10,19 @@ residencia = input("ingresa tu lugar de residencia: ")
 print(f"Soy {nombre}{apellido}, tengo {edad} años y vivo en {residencia}.")
 
 import match
-radio_str = input("Ingresar el radio del círculo: ")
+radio_str = input("Ingresar el radio del circulo: ")
 radio = float(radio_str)
 area = match.pi * (radio ** 2)
 perimetro = 2 * match.pi * radio
-print (f"El área del círculo es: {area}")
-print(f"El perímetro del círculo es: {perimetro}")
+print (f"El area del circulo es: {area}")
+print(f"El perimetro del circulo es: {perimetro}")
 
 segundos_str = input("Ingresar una cantidad de segundos: ")
 segundos = int(segundos_str)
 horas = segundos / 3600
 print(f"{segundos} segundos equivalen a {horas} horas.")
 
-numero = int(input("Ingresar un número para ver su tabla de multiplicar: "))
+numero = int(input("Ingresar un numero para ver su tabla de multiplicar: "))
 print(f"--- Tabla del {numero}---")
 print(f"{numero} x 1 = {numero * 1}")
 print(f"{numero} x 2 = {numero * 2}")
@@ -35,8 +35,8 @@ print(f"{numero} x 8 = {numero * 8}")
 print(f"{numero} x 9 = {numero * 9}")
 print(f"{numero} x 10 = {numero * 10}")
 
-num1 = int(input("Ingresar el priumer número entero (destino de 0): "))
-num2 = int(input("Ingresar el segundo número entero (destino de 0):"))
+num1 = int(input("Ingresar el priumer numero entero (destino de 0): "))
+num2 = int(input("Ingresar el segundo numero entero (destino de 0):"))
 suma = num1 + num2
 resta = num1 - num2
 multiplicacion = num1 * num2
@@ -55,8 +55,8 @@ celsius = float(input("Ingresa la temperatura en grados Celsius: "))
 fahrenheit = (9/5) * celsius + 32
 print(f"{celsius}°C equivalen a {fahrenheit}°F.")
 
-num1 = float(input("Ingresa el primer número: "))
-num2 = float(input("Ingresa el segundo número: "))
-num3 = float(input("Ingresa el tercer número: "))
+num1 = float(input("Ingresa el primer numero: "))
+num2 = float(input("Ingresa el segundo numero: "))
+num3 = float(input("Ingresa el tercer numero: "))
 promedio = (num1 + num2 + num3) / 3
-print(f"El promedio de los tres números es: {promedio}")
+print(f"El promedio de los tres numeros es: {promedio}")
